b946f789f9bd5928f8f90796ffe22163db8cb8c2
Pushpak-Jaiswal <pushpakmjaiswal@gmail.com>
2025-10-09 13:29:49 +0530

    updates for cors in middleware main.py

--- models/server/main.py
+++ models/server/main.py
@@ -1,10 +1,21 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.forest_router import router as forest_router
 from routers.seeds_router import router as seeds_router
 from routers.mushroom_router import router as mushroom_router
 
 app = FastAPI(title="Forest Cover Prediction API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],           
+    allow_headers=["*"],            
+)
+
+
 app.include_router(forest_router)
 app.include_router(seeds_router)
 app.include_router(mushroom_router)
